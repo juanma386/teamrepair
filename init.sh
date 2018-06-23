cf2f1d1a624091734fe6e5af8c31426ffc828144
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2018-06-22 22:55:20 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,43 +8,43 @@ echo '
 version 0.0.1 teamrepair de juanma386 22-06-2018
 #############################'
 sudo -v
-DIA=`date +"%d/%m/%Y"`
-HORA=`date +"%H:%M"`
-echo "
+DIA=`date +'%d/%m/%Y'`
+HORA=`date +'%H:%M'`
+echo '
 #############################
 Hoy es el $DIA y la hora actual es $HORA!
-#############################"
+#############################'
 wget -v https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-echo "Hoy $DIA y a la hora $HORA el sistema instala ha descargado teamviewer en sistema debian 9 64_86"
-echo "se procedera a realizar copia de respaldo del log de operaciones"
+echo 'Hoy $DIA y a la hora $HORA el sistema instala ha descargado teamviewer en sistema debian 9 64_86'
+echo 'se procedera a realizar copia de respaldo del log de operaciones'
 sudo teamviewer ziplog 
-echo "se procedera a realizar la eliminacion del software con defectos [teamviewer]"
+echo 'se procedera a realizar la eliminacion del software con defectos [teamviewer]'
 clear
-echo "
+echo '
 #############################
 Parando servicio comprometido
-#############################"
+#############################'
 sudo teamviewer daemon stop
-echo "
+echo '
 #############################
 Anulando servicio comprometido
-#############################"
+#############################'
 sudo teamviewer daemon disable
-echo "
+echo '
 #############################
 instalando herramientas para limpieza
-#############################"
+#############################'
 sudo apt install deborphan -y
-echo "
+echo '
 #############################
 aplicando limpieza
 #############################
-"
+'
 # Realiza limpieza dentro del sistema
 sudo deborphan -Z
 sudo apt-get clean -y
 sudo apt-get autoremove -y
-UNUSCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
+UNUSCONF=$(dpkg -l|grep '^rc'|awk '{print $2}')
 sudo apt-get remove --purge $UNUSCONF -y
 sudo apt-get install --reinstall deborphan -y
 sudo deborphan -Z
